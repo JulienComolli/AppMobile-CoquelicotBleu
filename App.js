5f2a7f7b7a6faa34eb576547de414e6767378c67
Alexis LEON <leonalex@free.fr>
2022-02-19 19:58:57 +0100

    ajout navigation

--- App.js
+++ App.js
@@ -1,10 +1,68 @@
-import { Text } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+function HomeView({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Coquelicot Bleu</Text>
+      <Text>L'encyclopédie des fleurs</Text>
+      <Button
+        title='Ajouter en favori'
+        onPress={() => navigation.navigate('AddFav')}
+      />
+      <Button
+        title='Consulter les favoris'
+        onPress={() => navigation.navigate('Fav')}
+      />
+      <Button
+        title='Ajouter une fleur'
+        onPress={() => navigation.navigate('AddFlower')}
+      />
+    </View>
+  );
+}
+
+function AddFavView() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Ajouter en favori</Text>
+    </View>
+  );
+}
+
+function FavView() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Voir les favoris</Text>
+      <Button
+        title='Ajouter de nouvelles fleurs en favori'
+        onPress={() => navigation.navigate('AddFav')}
+      />
+    </View>
+  );
+}
+
+function AddFlowerView() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Ajouter une fleur non-connue</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-
-    <Text> Julien je te déteste!!! </Text>
-
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeView} />
+        <Stack.Screen name="AddFav" component={AddFavView} />
+        <Stack.Screen name="Fav" component={FavView} />
+        <Stack.Screen name="AddFlower" component={AddFlowerView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
