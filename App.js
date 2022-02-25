ecc65a2924937edd58cc070e55a075f833ce2f2d
JulienComolli <comolli.julien@gmail.com>
2022-02-25 15:11:57 +0100

    MAJ du Gitignore

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Mycom
+// Mon commentaire
 
 function HomeView({ navigation }) {
   return (
