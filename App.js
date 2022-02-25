6c6b93034897ccac1ba5da0e64f4b2552a2e8e45
JulienComolli <comolli.julien@gmail.com>
2022-02-25 16:44:09 +0100

    Test des composants

--- App.js
+++ App.js
@@ -1,34 +1,43 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import Miaou from "./components/Miaou";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 // Mon commentaire
+const styles = StyleSheet.create({
+  Text: {
+    color: "#00ff66",
+  },
+});
 
 function HomeView({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Coquelicot Bleu</Text>
       <Text>L'encyclopédie des fleurs</Text>
       <Button
-        title='Ajouter en favori'
-        onPress={() => navigation.navigate('AddFav')}
+        title="Ajouter en favori"
+        onPress={() => navigation.navigate("AddFav")}
       />
       <Button
-        title='Consulter les favoris'
-        onPress={() => navigation.navigate('Fav')}
+        title="Consulter les favoris"
+        onPress={() => navigation.navigate("Fav")}
       />
       <Button
-        title='Ajouter une fleur'
-        onPress={() => navigation.navigate('AddFlower')}
+        title="Ajouter une fleur"
+        onPress={() => navigation.navigate("AddFlower")}
       />
+      <Miaou/>
     </View>
   );
 }
 
 function AddFavView() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Ajouter en favori</Text>
     </View>
   );
@@ -36,11 +45,11 @@ function AddFavView() {
 
 function FavView() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Voir les favoris</Text>
       <Button
-        title='Ajouter de nouvelles fleurs en favori'
-        onPress={() => navigation.navigate('AddFav')}
+        title="Ajouter de nouvelles fleurs en favori"
+        onPress={() => navigation.navigate("AddFav")}
       />
     </View>
   );
@@ -48,7 +57,7 @@ function FavView() {
 
 function AddFlowerView() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Ajouter une fleur non-connue</Text>
     </View>
   );
---
+++ components/Miaou.js
@@ -0,0 +1,19 @@
+//import react from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const Miaou = () => {
+  return (
+    <View>
+      <Text>Miaou !</Text>
+    </View>
+  );
+};
+
+const meowStyle = StyleSheet.create({
+  meowText: {
+    color: "salmon",
+    fontSize: 43,
+  },
+});
+
+export default Miaou;
