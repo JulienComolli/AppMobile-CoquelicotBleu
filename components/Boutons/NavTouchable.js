57152473ca437e6b01889a6b5a69a68a855b49db
selimhaddioui <selim.haddioui@hotmail.com>
2022-03-13 01:21:34 +0100

    New NavTouchable. It

---
+++ components/Boutons/NavTouchable.js
@@ -0,0 +1,44 @@
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
+const NavTouchable = ({ text, screen, touchableStyle, textStyle }) => {
+    const navigation = useNavigation();
+    const styles = StyleSheet.create({
+        touchable: { 
+            ...defaultStyles.touchable,
+            ...touchableStyle
+        },
+        text: {
+            ...defaultStyles.text,
+            ...textStyle
+        }
+    });
+    return (
+        <TouchableOpacity
+            style={styles.touchable}
+            onPress={() => { navigation.navigate(screen) }}
+        >
+            <Text style={styles.text}>
+                {text}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+export default NavTouchable;
+
+const defaultStyles = {
+    touchable: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 10
+    },
+    text: {
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+};
